788e1cc471b3784e642fbe397db753cf45280abe
Marco Maccaferri <marco.maccaferri@maccasoft.com>
2023-02-08 10:06:20 +0100

    Version update

--- src/com/maccasoft/propeller/SpinTools.java
+++ src/com/maccasoft/propeller/SpinTools.java
@@ -98,7 +98,7 @@ import jssc.SerialPortException;
 public class SpinTools {
 
     public static final String APP_TITLE = "Spin Tools";
-    public static final String APP_VERSION = "0.20.0";
+    public static final String APP_VERSION = "0.20.1";
 
     static final File defaultSpin1Examples = new File(System.getProperty("APP_DIR"), "examples/P1").getAbsoluteFile();
     static final File defaultSpin2Examples = new File(System.getProperty("APP_DIR"), "examples/P2").getAbsoluteFile();
