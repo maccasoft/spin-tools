da4bcfd42441fd0a4160953297a48f2a1ccfa55c
Marco Maccaferri <marco.maccaferri@maccasoft.com>
2024-03-26 11:06:58 +0100

    Update to Eclipse SWT 2024-03

--- modules/spin-tools/src/com/maccasoft/propeller/SpinTools.java
+++ modules/spin-tools/src/com/maccasoft/propeller/SpinTools.java
@@ -108,7 +108,7 @@ import jssc.SerialPortException;
 public class SpinTools {
 
     public static final String APP_TITLE = "Spin Tools IDE";
-    public static final String APP_VERSION = "0.34.2";
+    public static final String APP_VERSION = "0.35.0";
 
     static final File defaultSpin1Examples = new File(System.getProperty("APP_DIR"), "examples/P1").getAbsoluteFile();
     static final File defaultSpin2Examples = new File(System.getProperty("APP_DIR"), "examples/P2").getAbsoluteFile();
