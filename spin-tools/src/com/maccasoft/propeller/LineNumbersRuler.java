4b930268d8bf86f421f313f45d38a998f6199650
Marco Maccaferri <marco.maccaferri@maccasoft.com>
2026-03-02 09:38:30 +0100

    Replaced bookmark icons

--- spin-tools/src/com/maccasoft/propeller/LineNumbersRuler.java
+++ spin-tools/src/com/maccasoft/propeller/LineNumbersRuler.java
@@ -1,11 +1,10 @@
 /*
- * Copyright (c) 2021-25 Marco Maccaferri and others.
+ * Copyright (c) 2021-26 Marco Maccaferri and others.
  * All rights reserved.
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License v1.0 which accompanies this
- * distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
 package com.maccasoft.propeller;
@@ -204,8 +203,8 @@ public class LineNumbersRuler {
 
             for (int num = 0; num < bookmarks.length; num++) {
                 if (bookmarks[num] != null && bookmarks[num] == lineNumber) {
-                    Image image = ImageRegistry.getImageFromResources(String.format("notification-counter-%02d.png", num + 1));
-                    gc.drawImage(image, 0, y);
+                    Image image = ImageRegistry.getImageFromResources(String.format("%d.png", num + 1));
+                    gc.drawImage(image, 2, y);
                     break;
                 }
             }
